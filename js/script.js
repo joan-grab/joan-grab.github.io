62cecf5d59ba9ebd96fa7bf1ef2db1b0bd4fdac2
Asia <joan.grab@gmail.com>
2021-04-03 18:35:39 +0200

    get parameter url

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(function(){
+    if ($(".pictures-container").length) {
+        $(".pictures-container").on("click", ".overlay", function () {
+            let category = $(this).attr("data-filter");
+            console.log(category);
+            window.location.href = "./portfolio.html?cat="+category;
+        });
+    }
+})
